f3920b2871836e9d8c3b1945629129d493e71a0b
edujtm <eduzemacedo@hotmail.com>
2018-11-17 15:59:20 -0300

    Constructed django-admin site, models for the objects, function based views and class based views for author and books.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,118 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    """
+        Modelo que representa um genero de livro.
+    """
+    name = models.CharField(max_length=200, help_text='Digite um genero de livro (e.g. Ficcao Cientifica)')
+
+    def __str__(self):
+        """
+            String que representa o modelo de generos.
+        """
+        return self.name
+
+
+class Book(models.Model):
+    """
+        Modelo que representa um livro (mas não uma cópia específica dele presente na biblioteca).
+    """
+    title = models.CharField(max_length=200)
+
+    # Relacao One to Many entre os livros e os autores - O livro possuira um autor mas o autor poderá ter vários livros
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='Digite uma breve descricao do livro')
+    isbn = models.CharField('ISBN', max_length=13, help_text='<a href="https://www.isbn-international.org/content/what-isbn">Numero ISBN</a> de 13 caracteres.')
+
+    genre = models.ManyToManyField(Genre, help_text='Selecione um genero para este livro.')
+
+    def __str__(self):
+        """
+            String que representa o modelo de livros
+        """
+        return self.title
+
+    def get_absolute_url(self):
+        """
+            Retorna a url de acesso aos detalhes do livro selecionado.
+        """
+        return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ", ".join((genre.name for genre in self.genre.all()[:3]))
+
+    display_genre.short_description = 'Genre'
+
+
+class BookInstance(models.Model):
+    """
+        Modelo que representa um copia fisica do livro.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='ID exclusivo para esta cópia do livro em toda a biblioteca')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    written_in = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+    LOAN_STATUS = (
+        ('m', 'Manutencao'),
+        ('e', 'Emprestado'),
+        ('d', 'Disponivel'),
+        ('r', 'Reservado'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Disponibilidade do livro')
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """
+            String que representa o modelo de instancia do livro.
+        """
+        return f'{self.id} ({self.book.title})'
+
+
+class Author(models.Model):
+    """
+        Modelo que representa um autor.
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+    
+    def get_absolute_url(self):
+        """
+            Retorna a url de acesso aos detalhes do autor.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+    
+    def __str__(self):
+        """
+            String de representacao do autor.
+        """
+        return f'{self.last_name}, {self.first_name}'
+
+
+class Language(models.Model):
+    """
+        Modelo que representa a linguagem em que o livro foi escrito.
+    """
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        """
+            Retorna uma String que representa o livro.
+        """
+        return f'{self.name} ({self.country})'
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.views import generic
+from catalog.models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
+def index(request):
+    """
+        Função view da home do site.
+    """
+
+    # Gera contagems para os objetos principais
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Quantidade de livros disponíveis
+    num_instances_available = BookInstance.objects.filter(status__exact='d').count()
+
+    # Numero de livros com sophie no nome
+    num_sophie = Book.objects.filter(title__icontains='sophie').count()
+
+    num_science = Genre.objects.filter(name__icontains='science').count()
+
+    # funcao all() implicita
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books' : num_books,
+        'num_instances' : num_instances,
+        'num_instances_available' : num_instances_available,
+        'num_authors' : num_authors,
+        'num_sophie': num_sophie,
+        'num_science' : num_science,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
